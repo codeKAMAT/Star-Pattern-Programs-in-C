32c468b4c6d130ba626ccf9c6acb19eaa68fbef0
bera5186 <rbasu611@gmail.com>
2018-07-22 17:51:39 +0530

    commented

--- Alphabetical_Patterns/1. Making a pyramid of A .c
+++ Alphabetical_Patterns/1. Making a pyramid of A .c
@@ -11,15 +11,15 @@ UNTILL THE USER WANTS
 # include <stdio.h>
 
 int main(){
-    char character = 'A';
+    char character = 'A'; // character approach assining a variable
     int rows ; 
-    printf("enter the number of rows : ");
-    scanf("%d",&rows);
+    printf("enter the number of rows : "); // asking for input
+    scanf("%d",&rows); // user input
     printf("\n");
-    for(int i = 1 ; i<=rows ; i++){
-        for(int j = 1 ; j<=i ; j++){
-            printf("%c",character);
+    for(int row = 1 ; i<=rows ; i++){ // loop for iterating through rows
+        for(int columns = 1 ; j<=i ; j++){ // loop for iterating through columns
+            printf("%c",character); // printing the charcter
         }
-        printf("\n");
+        printf("\n"); // for changing line
     }
 }
